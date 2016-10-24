b7294500d222505dcbdbb257b42ea889c4521ab3
ASTRIRESTARI <astrirestari123@gmail.com>
2016-10-24 18:01:14 +0700

    versi 1.0.0

---
+++ app/src/main/java/com/example/astri/stuffies/SignIn.java
@@ -0,0 +1,16 @@
+package com.example.astri.stuffies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by astri on 24/10/2016.
+ */
+public class SignIn extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
+
